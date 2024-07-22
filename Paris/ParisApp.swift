90a1ee6cf348d2ae041f64a287a06e083041e8b9
Thejesh <45538592+Thejesh-404@users.noreply.github.com>
2024-07-22 00:15:41 -0700

    Initial Commit

---
+++ Paris/ParisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParisApp.swift
+//  Paris
+//
+//  Created by Thejesh on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ParisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
